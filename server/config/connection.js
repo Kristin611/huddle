f8002489dcfa1d478e82c3981f12e6f23b61033b
Kris10 <kmrichie611@gmail.com>
2024-04-15 16:49:24 -0500

    edit connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,6 +1,7 @@
+require('dotenv').config();
+
 const Sequelize = require('sequelize');
 
-require('dotenv').config();
 
 let sequelize;
 if (process.env.JAWSDB_URL) {
@@ -11,7 +12,7 @@ if (process.env.JAWSDB_URL) {
     process.env.DB_USER,
     process.env.DB_PASSWORD,
     {
-      host: 'localhost',
+      host: '127.0.0.1',
       dialect: 'mysql',
       port: 3306
     }
