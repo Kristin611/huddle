1645a5d4be08c43a2b1c182bb4cc7427fe38d477
Kris10 <kmrichie611@gmail.com>
2024-07-10 14:55:21 -0500

    added info to footer

--- client/src/components/Footer/index.jsx
+++ client/src/components/Footer/index.jsx
@@ -1,14 +1,18 @@
-import React from 'react'
+import React from 'react';
+import './Footer.css';
+import huddleLogo from '../../assets/Logo_2.png';
 
 const Footer = () => {
   return (
     <>
         <footer>
             <div className='footer-container'>
-                <button>Contact</button>
-            </div>
-            <div>
-                <p>&copy; 2024 Huddle. All rights reserved.</p>
+                <div>
+                    <p>&copy; 2024 Huddle. All rights reserved.</p>
+                </div>
+                <div>
+                    <img className='footer-logo' src={huddleLogo} alt="cartoon penguin"/>
+                </div>  
             </div>
         </footer>
     </>
