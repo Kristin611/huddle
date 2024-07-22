ad93b90221c38c7e575caa0eb482bf0c33ff16bd
Kris10 <kmrichie611@gmail.com>
2024-07-22 15:41:35 -0500

    updated server.js to serve files from dist instead of build

--- server/server.js
+++ server/server.js
@@ -33,7 +33,7 @@ app.use(routes);
 
 
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '../client/build')));
+    app.use(express.static(path.join(__dirname, '../client/dist')));
   } else {
     app.use(express.static(path.join(__dirname, '../client/public')))
   }
